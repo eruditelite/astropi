85eac6e417d483c23942cc79c7be50ad42e22831
John Jacques <eruditelite@gmail.com>
2016-03-24 15:05:01 -0500

    Make the destination address a command line argument.

--- huang_intro_to_bluetooth/rfcomm-client.c
+++ huang_intro_to_bluetooth/rfcomm-client.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
@@ -8,7 +9,16 @@ int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
     int s, status;
-    char dest[18] = "01:23:45:67:89:AB";
+
+    if (2 != argc) {
+      fprintf(stderr, "usage: %s <BD Address>\n", argv[0]);
+      exit(1);
+    }
+
+    if (17 != strlen(argv[1])) {
+      fprintf(stderr, "Bad BD Address: %lu/%s\n", strlen(argv[1]), argv[1]);
+      exit(1);
+    }
 
     // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
@@ -16,7 +26,7 @@ int main(int argc, char **argv)
     // set the connection parameters (who to connect to)
     addr.rc_family = AF_BLUETOOTH;
     addr.rc_channel = (uint8_t) 1;
-    str2ba( dest, &addr.rc_bdaddr );
+    str2ba( argv[1], &addr.rc_bdaddr );
 
     // connect to server
     status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
